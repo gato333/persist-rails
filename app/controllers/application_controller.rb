e83f44c25652c9fc4a2afce9cd8eeacffdb11b6b
Gato <gvoll333@gmail.com>
2014-12-03 17:59:43 -0500

    updated the root route to be to the post index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-      render text: "¡Esta puta Mundo es para los perros!"
-  end
-  def goodbye
-      render text: "fuck all of you, everyone!"
-  end
 end
